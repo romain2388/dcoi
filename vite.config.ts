6b79c25f55e556dcf16ed3058278efbf972ce150
Romain <romain@wylog.com>
2025-10-31 17:06:40 +0100

    first deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
-    server: {
-        port: 3000,
-    },
-    plugins: [
-        tsConfigPaths(),
-        tanstackStart(),
-        // react's vite plugin must come after start's vite plugin
-        viteReact(),
-    ],
-})
+  server: {
+    port: Number(process.env.PORT) || 3000,
+    host: true,
+  },
+  plugins: [tsConfigPaths(), tanstackStart(), viteReact()],
+});
