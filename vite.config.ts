7f09e2c5e52c4965e529eb43736814240dd57711
Romain <romain@wylog.com>
2025-10-31 17:21:25 +0100

    first deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
   server: {
     port: Number(process.env.PORT) || 3000,
     host: true,
   },
-  plugins: [tsConfigPaths(), tanstackStart(), viteReact()],
+  plugins: [tsConfigPaths(), tanstackStart(), nitroV2Plugin(), viteReact()],
 });
